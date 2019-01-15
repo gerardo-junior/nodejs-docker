c688e65259a00cb270172ed18f1699b5e9243469
Gerardo Junior <me@gerardo-junior.com>
2019-01-15 12:30:41 -0300

    Revert to old way to install node_modules

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 sudo chgrp -Rf ${USER} ${WORKDIR}
 
-if [ -e "$(pwd)/package.json" ]; then
-    /usr/local/bin/npm install
+if [ ! -d "$(pwd)/node_modules" ]; then
+    if [ -e "$(pwd)/yarn.lock" ]; then
+        /usr/local/bin/yarn install --pure-lockfile
+        /usr/local/bin/yarn cache clean --force
+    elif [ -e "$(pwd)/package.json" ]; then
+        /usr/local/bin/npm install
+        /usr/local/bin/npm cache clean --force
+    fi
 fi
 
 export PATH="${PATH}:${WORKDIR}/node_modules/.bin"
