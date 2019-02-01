27a5f56a291fcaa9ccdaee77b062053656844db8
Gerardo Junior <me@gerardo-junior.com>
2019-02-01 01:17:24 -0300

    Improving entrypoint

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-sudo chgrp -Rf ${USER} $(pwd)
+sudo chgrp -Rf ${USER} ${WORKDIR}
 
-if [ ! -d "$(pwd)/node_modules" ]; then
-    if [ -e "$(pwd)/yarn.lock" ]; then
+if [ ! -d "${WORKDIR}/node_modules" ]; then
+    if [ -e "${WORKDIR}/yarn.lock" ]; then
         /usr/local/bin/yarn install --pure-lockfile
         /usr/local/bin/yarn cache clean --force
-    elif [ -e "$(pwd)/package.json" ]; then
+    elif [ -e "${WORKDIR}/package.json" ]; then
         /usr/local/bin/npm install
         /usr/local/bin/npm cache clean --force
     fi
@@ -15,7 +15,7 @@ export PATH="${PATH}:${WORKDIR}/node_modules/.bin"
 
 if [ ! -z "$1" ]; then
     if [[ -z "$(which -- $1)" ]]; then
-        if [ -e "$(pwd)/yarn.lock" ]; then
+        if [ -e "${WORKDIR}/yarn.lock" ]; then
             /usr/local/bin/yarn "$@"
         else
             /usr/local/bin/npm run "$@"
